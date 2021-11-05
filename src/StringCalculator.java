c5c521d50f4e69dbfcd5f7be2280aada94c680e3
Alexander Andreyev <andreyev.sasha@gmail.com>
2021-11-05 19:10:04 +0200

    5 kata

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,6 +1,5 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
 
 public class StringCalculator{
@@ -8,24 +7,38 @@ public class StringCalculator{
         List<String> delimiters = new ArrayList<>();
         delimiters.add("\n");
         List<String> input;
-        List<String> numbers = new ArrayList<>();
+        List<String> numbers;
+
         if(exp.startsWith("//")) {
             input = Arrays.asList(exp.split("\n"));
             delimiters.add(input.get(0).replaceAll("//", ""));
             exp = exp.substring(exp.indexOf('\n')+1);
         }
+
         for(String d : delimiters) {
             exp = exp.replaceAll(d, ",");
         }
+
         numbers = Arrays.asList(exp.split(","));
-        List<Integer> list = new ArrayList<>();
+        List<Integer> resultList = new ArrayList<>();
+
+        List<Integer> negativeNumbers = new ArrayList<>();
         for(String s : numbers) {
-            list.add(Integer.parseInt(s));
+            int number = Integer.parseInt(s);
+            if(number < 0) {
+                negativeNumbers.add(number);
+            } else {
+                resultList.add(Integer.parseInt(s));
+            }
         }
-        int result = 0;
-        for(Integer i : list ) {
-            result += i;
+        if(!negativeNumbers.isEmpty()) {
+            throw new RuntimeException("Negative numbers in the expression: " + negativeNumbers);
+        } else {
+            int result = 0;
+            for (Integer i : resultList) {
+                result += i;
+            }
+            return result;
         }
-        return result;
     }
 }
