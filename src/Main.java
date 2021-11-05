9bd3e7af57cdd6836aa4a7473070332cd8c50648
Alexander Andreyev <andreyev.sasha@gmail.com>
2021-11-05 19:04:31 +0200

    4 kata

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         StringCalculator calc = new StringCalculator();
-        int result = calc.calc("1,2,3\n4");
+        int result = calc.calc("//;\n1,2;3\n4;5");
         System.out.println(result);
     }
 }
--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,10 +1,23 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 public class StringCalculator{
     public int calc(String exp) {
-        exp = exp.replaceAll("\n", ",");
-        String [] numbers = exp.split(",");
+        List<String> delimiters = new ArrayList<>();
+        delimiters.add("\n");
+        List<String> input;
+        List<String> numbers = new ArrayList<>();
+        if(exp.startsWith("//")) {
+            input = Arrays.asList(exp.split("\n"));
+            delimiters.add(input.get(0).replaceAll("//", ""));
+            exp = exp.substring(exp.indexOf('\n')+1);
+        }
+        for(String d : delimiters) {
+            exp = exp.replaceAll(d, ",");
+        }
+        numbers = Arrays.asList(exp.split(","));
         List<Integer> list = new ArrayList<>();
         for(String s : numbers) {
             list.add(Integer.parseInt(s));
