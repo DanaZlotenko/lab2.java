d18c1f8700c9a1cbfe33b01c398700faec752aea
Alexander Andreyev <andreyev.sasha@gmail.com>
2021-11-05 18:37:08 +0200

    2 kata

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         StringCalculator calc = new StringCalculator();
-        int result = calc.calc("1,2");
+        int result = calc.calc("1,2,3,4");
         System.out.println(result);
     }
 }
--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,8 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringCalculator{
     public int calc(String exp) {
         String [] numbers = exp.split(",");
-        int i = Integer.parseInt(numbers[0]);
-        int j= Integer.parseInt(numbers[1]);
-        return i + j;
+        List<Integer> list = new ArrayList<>();
+        for(String s : numbers) {
+            list.add(Integer.parseInt(s));
+        }
+        int result = 0;
+        for(Integer i : list ) {
+            result += i;
+        }
+        return result;
     }
 }
