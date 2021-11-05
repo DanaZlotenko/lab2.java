18654be5c06aa20da4470422c9685fa0617a8426
Alexander Andreyev <andreyev.sasha@gmail.com>
2021-11-05 20:21:55 +0200

    9 kata

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         StringCalculator calc = new StringCalculator();
         int result = calc.calc("//[***][%%%]\n1***8000,2***3\n4***5***2000%%%5");
+        System.out.println("Kata 9 was already done in kata 8");
         System.out.println(result);
     }
 }
